21d7224d2cef08b0e2067f3777b9a2570cf93b13
Kenneth Law <cyt05108@gmail.com>
2021-02-08 19:59:22 +0800

    :recycle: refactor and format

--- lib/sendcloud-sms.rb
+++ lib/sendcloud-sms.rb
@@ -5,10 +5,9 @@ require 'yaml'
 
 module SendCloud
   class SMS
-
     def self.load!(file, environment)
       config = YAML.load_file(file)
-      self.auth(config[environment.to_s]['user'], config[environment.to_s]['api_key'])
+      auth(config[environment.to_s]['user'], config[environment.to_s]['api_key'])
     end
 
     def self.auth(user, api_key)
@@ -19,12 +18,12 @@ module SendCloud
     def self.sign(template, phone, vars)
       param_str = "#{@api_key}&"
       {
-          smsUser: @user,
-          templateId: template,
-          msgType: 0,
-          phone: (phone.is_a?Array) ? phone.join(',') : phone,
-          vars: vars.to_json
-      }.sort {|a, b| a.to_s <=> b.to_s}.map { |item| param_str << "#{item[0]}=#{item[1]}&" }
+        smsUser: @user,
+        templateId: template,
+        msgType: 0,
+        phone: phone.is_a?(Array) ? phone.join(',') : phone,
+        vars: vars.to_json
+      }.sort_by(&:to_s).map { |item| param_str << "#{item[0]}=#{item[1]}&" }
       param_str << @api_key
       Digest::MD5.new.update(param_str)
     end
@@ -32,34 +31,53 @@ module SendCloud
     def self.sign_voice(phone, code)
       param_str = "#{@api_key}&"
       {
-          smsUser: @user,
-          phone: phone,
-          code: code
-      }.sort {|a, b| a.to_s <=> b.to_s}.map { |item| param_str << "#{item[0]}=#{item[1]}&" }
+        smsUser: @user,
+        phone: phone,
+        code: code
+      }.sort_by(&:to_s).map { |item| param_str << "#{item[0]}=#{item[1]}&" }
       param_str << @api_key
       Digest::MD5.new.update(param_str)
     end
 
-    def self.send(template, phone, vars)
-      signature = sign(template, phone, vars)
-      response = RestClient.post 'http://sendcloud.sohu.com/smsapi/send?',
-                                 smsUser: @user,
-                                 templateId: template,
-                                 msgType: 0,
-                                 phone: (phone.is_a?Array) ? phone.join(',') : phone,
-                                 vars: vars.to_json,
-                                 signature: signature
-      JSON.parse(response.to_s)['statusCode']
+    def self.send_sms(template, phone, vars)
+      request(
+        'smsapi/send?',
+        smsUser: @user,
+        templateId: template,
+        msgType: 0,
+        phone: phone.is_a?(Array) ? phone.join(',') : phone,
+        vars: vars.to_json,
+        signature: sign(template, phone, vars)
+      )
     end
 
     def self.send_voice(phone, code)
-      signature = sign_voice(phone, code)
-      response = RestClient.post 'http://sendcloud.sohu.com/smsapi/sendVoice?',
-                                 smsUser: @user,
-                                 phone: phone,
-                                 code: code,
-                                 signature: signature
-      JSON.parse(response.to_s)['statusCode']
+      request(
+        'smsapi/sendVoice?',
+        smsUser: @user,
+        phone: phone,
+        code: code,
+        signature: sign_voice(phone, code)
+      )
+    end
+
+    def self.request(path, **params)
+      json_resp = RestClient.post "http://sendcloud.sohu.com/#{path}", **params
+      resp = JSON.parse(json_resp.to_s)
+      status_code = resp['statusCode']
+      raise SendError.new(resp['message'], status_code) unless status_code == 200
+
+      resp['info']
+    end
+
+    class SendError < StandardError
+      attr_reader :message, :status_code
+
+      def initialize(message, status_code)
+        super("Sendcloud API Error (#{status_code}): #{message}")
+        @message = message
+        @status_code = status_code
+      end
     end
   end
 end
